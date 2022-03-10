19a521c8f4f791d79447f38eec73f092b0df363f
Laure MASSON <lmasson@e2r12p8.clusters.42paris.fr>
2022-03-10 17:26:20 +0100

    colors, pixel and angle

---
+++ srcs/pixel.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pixel.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmasson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 14:09:21 by lmasson           #+#    #+#             */
+/*   Updated: 2022/03/10 17:24:52 by lmasson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3D.h"
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
+
+	i = img->bpp - 8;
+	pixel = img->adrr + (y * img->line_len + x * (img->bpp / 8));
+	while (0 <= i)
+	{
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+	*(int *)pixel = color;
+}
